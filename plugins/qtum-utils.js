2d8f641195d6c46bdda1f588309538ac740f2fb4
Xuan Yan <xuanyan@xuanyan.ws>
2017-12-13 11:33:30 +0800

    currency delimiters

--- plugins/qtum-utils.js
+++ plugins/qtum-utils.js
@@ -2,11 +2,16 @@ import Vue from 'vue'
 import moment from 'moment'
 
 Vue.filter('qtum', (satoshis, precision = null) => {
+  function replace(string) {
+    return string.replace(/^(\d{1,3})((\d{3})*)(\.\d+|)$/g, (_, before, middle, __, after) => {
+      return before + middle.replace(/(\d{3})/g, ',$1') + after
+    })
+  }
   if (precision == null) {
     let s = satoshis.toString().padStart(9, '0')
-    return (s.slice(0, -8) + '.' + s.slice(-8)).replace(/\.?0*$/g, '')
+    return replace((s.slice(0, -8) + '.' + s.slice(-8)).replace(/\.?0*$/g, ''))
   } else {
-    return (satoshis / 1e8).toFixed(precision)
+    return replace((satoshis / 1e8).toFixed(precision))
   }
 })
 
