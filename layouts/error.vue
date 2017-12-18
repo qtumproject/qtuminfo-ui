94901302da18b51e37cfbfc0fc28cdfbd319394c
Xuan Yan <xuanyan@xuanyan.ws>
2017-12-18 13:57:43 +0800

    custom error page

---
+++ layouts/error.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <header class="container">
+      <Navigator></Navigator>
+    </header>
+    <div class="error-container">
+      <div class="has-text-centered">
+        <p class="is-size-3">{{ error.message }}</p>
+        <br>
+        <nuxt-link to="/" class="button is-primary is-size-5">Back to Home Page</nuxt-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Navigator from '@/components/nav.vue'
+
+  export default {
+    props: ['error'],
+    components: {Navigator}
+  }
+</script>
+
+<style scoped>
+  #app {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+  }
+  .error-container {
+    flex: auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
