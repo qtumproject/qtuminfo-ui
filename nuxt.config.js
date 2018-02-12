2e5dae157927e1e5f62f4faf6ad9ac286e0d5a3d
Xuan Yan <xuanyan@xuanyan.ws>
2018-02-12 18:08:38 +0800

    user-scalable=no

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ module.exports = {
   head: {
     meta: [
       {charset: 'utf-8'},
-      {name: 'viewport', content: 'width=device-width, initial-scale=1'}
+      {name: 'viewport', content: 'width=device-width, initial-scale=1, user-scalable=no'}
     ]
   },
   css: [
