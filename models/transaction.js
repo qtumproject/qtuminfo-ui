b458068861f711ae81aa6b4e5f5a97b09f52cc6e
Xuan Yan <xuanyan@xuanyan.ws>
2018-03-11 21:56:31 +0800

    fix bugs when no querying for zero transactions

--- models/transaction.js
+++ models/transaction.js
@@ -3,7 +3,11 @@ import * as QtuminfoAPI from '@/services/qtuminfo-api'
 class Transaction {
   static get(id) {
     if (Array.isArray(id)) {
-      return QtuminfoAPI.get('/txs/' + id.join(','))
+      if (id.length === 0) {
+        return []
+      } else {
+        return QtuminfoAPI.get('/txs/' + id.join(','))
+      }
     } else {
       return QtuminfoAPI.get(`/tx/${id}`)
     }
