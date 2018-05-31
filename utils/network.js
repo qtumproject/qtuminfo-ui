39688f63caed81b1d8426e86c24e1e0f1c6debd9
Xuan Yan <xuanyan@xuanyan.ws>
2018-05-31 22:53:37 +0800

    change segwit address prefix

--- utils/network.js
+++ utils/network.js
@@ -3,14 +3,14 @@ export default {
     pubkey: 0x3a,
     pubkeyhash: 0x3a,
     scripthash: 0x32,
-    witness_v0_keyhash: 'bc',
-    witness_v0_scripthash: 'bc'
+    witness_v0_keyhash: 'qc',
+    witness_v0_scripthash: 'qc'
   },
   testnet: {
     pubkey: 0x78,
     pubkeyhash: 0x78,
     scripthash: 0x6e,
-    witness_v0_keyhash: 'tb',
-    witness_v0_scripthash: 'tb'
+    witness_v0_keyhash: 'tq',
+    witness_v0_scripthash: 'tq'
   }
 }[process.env.network || 'mainnet']
